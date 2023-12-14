47da392e12c38011e0a494b1d08860f9d72adee3
unknown <berlin.eminem@gmail.com>
2023-12-13 17:56:27 -0800

    first commit

---
+++ PARTIE_PRATIQUE/VarExo1.java
@@ -0,0 +1,22 @@
+public class VarExo1 {
+
+        public static void main(String[] args) {
+            int a = 5;
+            int b = 10;
+
+            System.out.println("Avant l'échange :");
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+
+            // Échange sans variable temporaire à l'aide de XOR
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+
+            System.out.println("\nAprès l'échange :");
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+        }
+
+
+}
